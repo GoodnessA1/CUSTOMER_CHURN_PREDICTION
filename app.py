f39c2bd4cffe34cbd78756f093afc269eca08997
GoodnessA1 <goodnessabisoye@gmail.com>
2024-12-30 01:45:29 +0100

    new

--- app.py
+++ app.py
@@ -9,3 +9,25 @@ def home():
 
 if '__main__' == __name__:
     app.run(host='0.0.0.0', debug=True, port=8080)
+
+
+'''
+Gender: Male, Female
+SeniorCitizen: 0, 1
+Partner: Yes, No
+Dependents: Yes, No
+Tenure: integer
+PhoneService, Yes, No
+MultipleLines: Yes, No
+InternetService: DSL, Fiber optic, No
+OnlineSecurity: Yes, No
+OnlineBackup: Yes, No
+DeviceProtection: Yes, No
+TechSupport: Yes, No
+StreamingTV: Yes, No
+StreamingMovies: Yes, No
+Contract: Monthly, One year, Two year
+PaperlessBilling: Yes, NO	
+PaymentMethod: Manual, Bank transfer(automatic), Credit card (automatic)
+MonthlyCharges: float
+'''
